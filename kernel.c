a21c82c6671ab2b5970ed1599d77bb2c767bb718
cr0sd <coreydunn92@outlook.com>
2020-05-28 22:11:44 -0500

    kernel: FILE struct

--- kernel.c
+++ kernel.c
@@ -1,12 +1,45 @@
 #define SPACE "                                                                                                "
-void puts(char*s)
+
+// FILE type
+typedef struct FILE
 {
-	static unsigned short*vram=(unsigned short*)0xb8000;
-	for(int i=0;s[i];++i)
-		vram[i]=0x1f00|s[i];
+	unsigned fd;
+	unsigned short*offset;
+	unsigned int seek;
+} FILE;
+
+FILE*stdout=&(FILE){.fd=0,.offset=(unsigned short*)0xb8000,.seek=0};
+
+// puts to FILE
+void fputs(FILE*f,char*s)
+{
+	for(unsigned i=0;s[i];++i)
+		*f->offset++=0x1f00|s[i];
 }
 
+#define puts(x) fputs(stdout,x)
+
+void putchar(FILE*f,char c)
+{
+	*f->offset++=0x1f00|c;
+}
+
+// print integer
+void printif(FILE*f,int d)
+{
+	while(d>0)
+	{
+		int m=d%10;
+		d/=10;
+		putchar(f,m);
+	}
+}
+
+#define printi(x) printif(stdout,x)
+
+// Entry point
 void kernelmain(void*multiboot_structure,unsigned magicnumber)
 {
 	puts("Hello and welcome to my first operating system, yo!"SPACE);
+	printi(101);
 }
